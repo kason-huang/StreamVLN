ddc20626fc6ad4eff96078adc4f1f02be296467c
kason-huang <qinkai1994@qq.com>
2025-12-10 08:40:02 +0000

    fix: update graident_auumulation_step

--- scripts/streamvln_train_objnav_h20.sh
+++ scripts/streamvln_train_objnav_h20.sh
@@ -159,7 +159,7 @@ torchrun --nnodes=$NNODES --nproc_per_node=$NPROC_PER_NODE \
     --num_train_epochs 1 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 4 \
+    --gradient_accumulation_steps 2 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 2000 \
